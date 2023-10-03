02929fb60d67920a2a4b0ea2edeb41a7eb1fa942
Modou Aicha Diop <modouaichadiop@gmail.com>
2023-10-03 20:07:11 +0000

    create a middleware and function for  errorHandler

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,16 +1,16 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const { username, firstname, lastname, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({ username, firstname, lastname, email, password: hashedPassword });
-    try{
+    try {
         await newUser.save();
         res.status(201).json("User Created successfully");
     }
-    catch(error){
-        res.status(500).json(error.message);
+    catch (error) {
+        next(error);
 
     }
 };
--- api/index.js
+++ api/index.js
@@ -18,10 +18,21 @@ mongoose.connect(process.env.MONGO)
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log('Server running on port 3000');
 }
 );
-app.use(express.json());
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
