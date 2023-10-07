32b6130f5fe404f3644860c6f2ff946fe14b9f5c
Modou Aicha Diop <modouaichadiop@gmail.com>
2023-10-07 12:57:06 +0000

    create user update info api

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
-
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
@@ -19,6 +19,8 @@ mongoose.connect(process.env.MONGO)
 const app = express();
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.listen(3000, () => {
     console.log('Server running on port 3000');
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,7 +1,13 @@
 import express from "express";
-import { test } from '../controllers/user.controller.js';
+import { test, updateUser } from '../controllers/user.controller.js';
+import { verifyToken } from "../utils/verifyUSer.js";
+
+
+
+
 const router = express.Router();
 
 router.get('/test', test);
+router.post('/update/:id', verifyToken, updateUser);
 
 export default router;
