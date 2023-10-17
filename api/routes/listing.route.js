37a38472b9f023a0b16eb25cd573537a9da223a1
Modou Aicha Diop <modouaichadiop@gmail.com>
2023-10-17 15:08:38 +0000

    delete listings

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createListing } from '../controllers/listing.controller.js';
+import { createListing, deleteListing } from '../controllers/listing.controller.js';
 import { verifyToken } from '../utils/verifyUSer.js';
 
 
@@ -7,5 +7,6 @@ const router = express.Router();
 
 
 router.post("/create", verifyToken, createListing);
+router.delete("/delete/:id", verifyToken, deleteListing);
 
 export default router;
