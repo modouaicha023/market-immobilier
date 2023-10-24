285817d67be8627e2fe6e8ac83a50e5c5f8dd969
Modou Aicha Diop <modouaichadiop@gmail.com>
2023-10-24 18:42:51 +0000

    adda boute page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-6 text-slate-800">
+        About Sama Keur.SN
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum
+        explicabo atque saepe amet dolore quaerat repudiandae delectus iusto
+        voluptatum, aliquam o Lorem ipsum dolor sit, amet consectetur
+        adipisicing elit. Quae, exercitationem aut deleniti iste nihil fugit
+        quibusdam provident eos, magni dolorum incidunt earum rem! Unde, dolores
+        quo facere laborum ullam quia? bcaecati, facere aut odio consectetur
+        perferendis culpa quam hic beatae? lor
+      </p>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias
+        officia numquam obcaecati, delectus eius velit a harum voluptatibus
+        tempore ad minus commodi, consectetur ut ducimus in voluptates voluptate
+        optio perferendis?
+      </p>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias
+        officia numquam obcaecati, delectus eius velit a harum voluptatibus
+        tempore ad minus commodi, consectetur ut ducimus in voluptates voluptate
+        optio perferendis?
+      </p>
+    </div>
+  );
 }
